f0980c0e1470bd7d15f7dc02a3be0e4067a2b55e
Engin Diri <engin.diri@ediri.de>
2025-05-14 16:07:39 +0200

    feat: add prettier and eslint support (#15)

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    prettierConfig,
+    eslintPluginPrettier,
+    {
+        ignores: ['node_modules/**', 'dist/**']
+    }
+);
